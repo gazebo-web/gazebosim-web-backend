784224ecb1a9ca1431e291f0f6034ea833238d34
ahcorde <ahcorde@gmail.com>
2020-08-21 17:06:58 +0200

    Added /tutorials resource to get all the available tutorials in the docs repository
    Signed-off-by: ahcorde <ahcorde@gmail.com>

---
+++ controllers/tutorials.go
@@ -0,0 +1,69 @@
+package controllers
+
+import (
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+
+	"github.com/jinzhu/gorm"
+	"gitlab.com/ignitionrobotics/web/ign-go"
+	"gopkg.in/yaml.v2"
+)
+
+
+// Docs returns a map of version names to array of tutorial names.
+func Tutorials(tx *gorm.DB, w http.ResponseWriter, r *http.Request) (interface{}, *ign.ErrMsg) {
+	// Get all subdirectories in the "docs" folder
+	// versionDirs, err := ioutil.ReadDir("docs")
+
+	type Page struct {
+		Name        string `json:"name" yaml:"name"`
+		Title       string `json:"title" yaml:"title"`
+		File        string `json:"file" yaml:"file"`
+		Description string `json:"description" yaml:"description"`
+		Children    []Page `json:"children" yaml:"children"`
+	}
+	type Pages struct {
+		Pages []Page `json:"pages" yaml:"pages"`
+	}
+
+	type RootIndex struct {
+		Pages    []Page   `json:"pages" yaml:"pages"`
+		Releases []string `json:"releases" yaml:"releases"`
+	}
+
+	type TutorialsInfo struct {
+		Versions []string          `json:"versions"`
+		Pages    map[string][]Page `json:"pages" yaml:"pages"`
+	}
+	var result TutorialsInfo
+
+	result.Pages = make(map[string][]Page)
+
+	var rootIndex RootIndex
+	file := filepath.Join("docs", "tutorials.yaml")
+	rootData, _ := ioutil.ReadFile(file)
+	err := yaml.Unmarshal(rootData, &rootIndex)
+
+	result.Pages["all"] = make([]Page, len(rootIndex.Pages))
+	copy(result.Pages["all"], rootIndex.Pages)
+
+	if err == nil {
+		// Iterate over the version directories
+		for _, v := range rootIndex.Releases {
+			// Get all files in the versioned directory
+			result.Versions = append(result.Versions, v)
+			file := filepath.Join("docs", v, "index.yaml")
+			data, _ := ioutil.ReadFile(file)
+
+			var pageData Pages
+			_ = yaml.Unmarshal(data, &pageData)
+			result.Pages[v] = make([]Page, len(pageData.Pages))
+			copy(result.Pages[v], pageData.Pages)
+		}
+	} else {
+		return result, ign.NewErrorMessageWithBase(ign.ErrorFileNotFound, err)
+	}
+
+	return result, nil
+}
--- routes.go
+++ routes.go
@@ -97,6 +97,42 @@ var Routes = ign.Routes{
 		SecureMethods: ign.SecureMethods{},
 	},
 
+	// Route for tutorials
+	ign.Route{
+		Name:        "Tutorials",
+		Description: "Routes for tutorials",
+		URI:         "/tutorials",
+		Headers:     ign.AuthHeadersOptional,
+		Methods: ign.Methods{
+			// swagger:route GET /tutorials tutorials listTutorials
+			//
+			// Get a list of tutorial pages
+			//
+			//   Produces:
+			//   - application/json
+			//
+			//   Schemes: https
+			//
+			//   Responses:
+			//     200: Libraries
+			ign.Method{
+				Type:        "GET",
+				Description: "Get list of tutorials pages",
+				Handlers: ign.FormatHandlers{
+					ign.FormatHandler{
+						Extension: ".json",
+						Handler:   ign.JSONListResult("", controllers.Tutorials),
+					},
+
+					ign.FormatHandler{
+						Extension: "",
+						Handler:   ign.JSONListResult("", controllers.Tutorials),
+					},
+				},
+			},
+		},
+		SecureMethods: ign.SecureMethods{},
+	},
 	// Route for documentation
 	ign.Route{
 		Name:        "Docs",
